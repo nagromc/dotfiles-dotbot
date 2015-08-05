1600135a9638beb48e2d5f4b7bd16a6e3c95988e
Morgan Courbet <nagromc@users.noreply.github.com>
2015-08-05 14:54:55 +0200

    Move fish related files in .config/fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # Load the shell dotfiles, and then some:
 # * ~/.path can be used to extend `$PATH`.
 # * ~/.extra can be used for other settings you don’t want to commit.
-for file in ~/.{path,exports,aliases,functions,extra};
+for file in ~/.config/fish/{path,exports,aliases,functions,extra};
     [ -r "$file" ] ; and [ -f "$file" ]; and . "$file"
 end
 set -e file
@@ -13,10 +13,10 @@ tmux attach; or tmux new
 
 # promptline.vim
 function fish_prompt
-    env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.shell_prompt.sh left
+    env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/shell_prompt.sh left
 end
 
 function fish_right_prompt
-    env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.shell_prompt.sh right
+    env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/shell_prompt.sh right
 end
 
