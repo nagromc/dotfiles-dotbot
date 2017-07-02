6b5123e68d01315e13f4c6979817594af7dfec69
Morgan Courbet <ping@morgan.ploki.fr>
2017-07-02 18:20:45 +0200

    Add bulk extention installation for VS Code on Windows

---
+++ code/install-extentions.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cmd /c code --install-extension dbaeumer.vscode-eslint
+cmd /c code --install-extension EditorConfig.editorconfig
+cmd /c code --install-extension HookyQR.beautify
+
+pause
