200a76128d08444ccf497a00d216e5bb5312e916
Morgan Courbet <ping@morgan.ploki.fr>
2017-07-02 17:37:31 +0200

    Add Brackets config for Windows

--- install.bat
+++ install.bat
@@ -5,6 +5,7 @@ call:link %USERPROFILE%\.atom\config.cson %CD%\atom\config.cson
 call:link %USERPROFILE%\.atom\init.coffee %CD%\atom\init.coffee
 call:link %USERPROFILE%\.atom\keymap.cson %CD%\atom\keymap.cson
 call:link %USERPROFILE%\.atom\snippets.cson %CD%\atom\snippets.cson
+call:link %APPDATA%\Brackets\brackets.json %CD%\brackets\brackets.json
 call:link %APPDATA%\Code\User\settings.json %CD%\code\settings.json
 call:link %USERPROFILE%\.editorconfig %CD%\editorconfig\editorconfig
 call:link %USERPROFILE%\.gitconfig %CD%\git\gitconfig
