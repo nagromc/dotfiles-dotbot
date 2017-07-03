47abcd3a60bae6ad7bb1c2b41471758469056287
Morgan Courbet <ping@morgan.ploki.fr>
2017-07-03 11:30:52 +0200

    Add --copy argument to install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,26 @@
 @echo off
 
-call:link %USERPROFILE%\.atom\apmrc %CD%\atom\apmrc
-call:link %USERPROFILE%\.atom\config.cson %CD%\atom\config.cson
-call:link %USERPROFILE%\.atom\init.coffee %CD%\atom\init.coffee
-call:link %USERPROFILE%\.atom\keymap.cson %CD%\atom\keymap.cson
-call:link %USERPROFILE%\.atom\snippets.cson %CD%\atom\snippets.cson
-call:link %APPDATA%\Brackets\brackets.json %CD%\brackets\brackets.json
-call:link %APPDATA%\Code\User\settings.json %CD%\code\settings.json
-call:link %USERPROFILE%\.editorconfig %CD%\editorconfig\editorconfig
-call:link %USERPROFILE%\.gitconfig %CD%\git\gitconfig
+:: if no arg, set it to "--link"
+set arg=%1
+if [%arg%]==[] (
+    set arg=--link
+)
+
+:: check if arg is "--link" or "--copy"
+if not [%arg%]==[--link] if not [%arg%]==[--copy]  (
+    echo The argument %arg% is not supported
+    goto:eof
+)
+
+call:link %USERPROFILE%\.atom\apmrc %CD%\atom\apmrc %arg%
+call:link %USERPROFILE%\.atom\config.cson %CD%\atom\config.cson %arg%
+call:link %USERPROFILE%\.atom\init.coffee %CD%\atom\init.coffee %arg%
+call:link %USERPROFILE%\.atom\keymap.cson %CD%\atom\keymap.cson %arg%
+call:link %USERPROFILE%\.atom\snippets.cson %CD%\atom\snippets.cson %arg%
+call:link %APPDATA%\Brackets\brackets.json %CD%\brackets\brackets.json %arg%
+call:link %APPDATA%\Code\User\settings.json %CD%\code\settings.json %arg%
+call:link %USERPROFILE%\.editorconfig %CD%\editorconfig\editorconfig %arg%
+call:link %USERPROFILE%\.gitconfig %CD%\git\gitconfig %arg%
 
 
 echo N | copy /-Y %CD%\git\gitconfig_local.template %USERPROFILE%\.gitconfig_local
@@ -19,6 +31,10 @@ echo.
 pause & goto:eof
 
 :link
-del %~1
-mklink %~1 %~2
+if [%~3]==[--link] (
+    del %~1
+    mklink %~1 %~2
+) else if [%~3]==[--copy] (
+    copy %~2 %~1
+)
 goto:eof
