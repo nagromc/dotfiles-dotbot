caeed31d89e412534efbff4f152c0c37c70fb7f8
Morgan Courbet <ping@morgan.ploki.fr>
2018-01-05 19:24:47 +0100

    Add Firefox

---
+++ firefox/user.js
@@ -0,0 +1,17 @@
+user_pref("browser.newtabpage.activity-stream.enabled", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", true);
+user_pref("browser.startup.homepage", "https://duckduckgo.com/?k5=2&kae=d&kaj=m&kak=-1&kam=osm&kap=-1&kaq=-1&kax=-1&kp=-2");
+user_pref("browser.startup.page", 3);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("general.warnOnAboutConfig", false);
+// https://feeding.cloud.geek.nz/posts/tweaking-cookies-for-privacy-in-firefox/
+user_pref("network.cookie.cookieBehavior", 3);
+user_pref("network.cookie.lifetime.days", 5);
+user_pref("network.cookie.lifetimePolicy", 3);
+user_pref("network.cookie.thirdparty.sessionOnly", true);
+// https://feeding.cloud.geek.nz/posts/tweaking-referrer-for-privacy-in-firefox/
+user_pref("network.http.referer.trimmingPolicy", 1);
+user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("privacy.firstparty.isolate", true);
+user_pref("security.webauth.u2f", true);
