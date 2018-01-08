a6dd1763effa31baa94c6b2aaf1044a9680bc855
Morgan Courbet <ping@morgan.ploki.fr>
2018-01-08 21:12:11 +0100

    Add more Firefox settings

--- firefox/user.js
+++ firefox/user.js
@@ -4,6 +4,10 @@ user_pref("browser.shell.checkDefaultBrowser", true);
 user_pref("browser.startup.homepage", "https://duckduckgo.com/?k5=2&kae=d&kaj=m&kak=-1&kam=osm&kap=-1&kaq=-1&kax=-1&kp=-2");
 user_pref("browser.startup.page", 3);
 user_pref("browser.urlbar.trimURLs", false);
+user_pref("devtools.cache.disabled", true);
+user_pref("devtools.theme", "dark");
+user_pref("devtools.webconsole.timestampMessages", true);
+user_pref("extensions.pocket.enabled", false);
 user_pref("general.warnOnAboutConfig", false);
 // https://feeding.cloud.geek.nz/posts/tweaking-cookies-for-privacy-in-firefox/
 user_pref("network.cookie.cookieBehavior", 3);
@@ -13,5 +17,7 @@ user_pref("network.cookie.thirdparty.sessionOnly", true);
 // https://feeding.cloud.geek.nz/posts/tweaking-referrer-for-privacy-in-firefox/
 user_pref("network.http.referer.trimmingPolicy", 1);
 user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.trackingprotection.enabled", true);
 user_pref("security.webauth.u2f", true);
