c033255a79bde5c9ea7f1ecc433ad820a338984b
Morgan Courbet <ping@morgan.ploki.fr>
2018-01-27 14:05:23 +0100

    Add shellcheck extension to VS Code

--- code/install-extentions.sh
+++ code/install-extentions.sh
@@ -3,5 +3,6 @@
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.editorconfig
 code --install-extension HookyQR.beautify
+code --install-extension timonwong.shellcheck
 code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-maven
