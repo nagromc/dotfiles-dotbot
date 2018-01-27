4243ec2c91dfbf579d4dc889d8452c980a7ef54c
Morgan Courbet <ping@morgan.ploki.fr>
2018-01-27 13:49:06 +0100

    Add more security and privacy-related options to Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,9 @@
+// Some interesting resources to pimp your Firefox configuration :
+// https://feeding.cloud.geek.nz/posts/tweaking-cookies-for-privacy-in-firefox/
+// https://feeding.cloud.geek.nz/posts/tweaking-referrer-for-privacy-in-firefox/
+// https://wiki.mozilla.org/Security/Cookies
+// https://wiki.mozilla.org/Security/Referrer
+
 user_pref("browser.newtabpage.activity-stream.enabled", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", true);
@@ -9,14 +15,15 @@ user_pref("devtools.theme", "dark");
 user_pref("devtools.webconsole.timestampMessages", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("general.warnOnAboutConfig", false);
-// https://feeding.cloud.geek.nz/posts/tweaking-cookies-for-privacy-in-firefox/
 user_pref("network.cookie.cookieBehavior", 3);
 user_pref("network.cookie.lifetime.days", 5);
 user_pref("network.cookie.lifetimePolicy", 3);
 user_pref("network.cookie.thirdparty.sessionOnly", true);
-// https://feeding.cloud.geek.nz/posts/tweaking-referrer-for-privacy-in-firefox/
 user_pref("network.http.referer.trimmingPolicy", 1);
 user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+user_pref("network.http.sendRefererHeader", 1);
+user_pref("network.http.sendSecureXSiteReferrer", false);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.trackingprotection.enabled", true);
