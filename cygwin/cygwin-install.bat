f0c47999a752bb1a3d8c789d558d1df0122b0fff
Morgan Courbet <ping@morgan.ploki.fr>
2018-03-06 14:50:07 +0100

    Add Cygwin installation

---
+++ cygwin/cygwin-install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set "cygPkgList=aspell,corkscrew,curl,fish,git,tig,tmux,vim,wget"
+
+set "cygMirrorSite=http://www.mirrorservice.org/sites/sourceware.org/pub/cygwin/"
+set "cygInstallerUrl=https://cygwin.com/setup-x86_64.exe"
+set "cygInstallerPath=%CD%\setup-x86_64.exe"
+set "cygBasePath=%CD%"
+set "cygOsPath=%cygBasePath%\cygwin-os"
+set "cygPkgPath=%cygBasePath%\cygwin-pkg"
+
+
+if not exist "%cygInstallerPath%" echo Please download cygwin from "%cygInstallerUrl%" first. && pause && exit
+
+"%cygInstallerPath%" --no-admin --quiet-mode --local-package-dir "%cygPkgPath%" --site "%cygMirrorSite%" --root "%cygOsPath%" --packages "%cygPkgList%"
+
+pause
