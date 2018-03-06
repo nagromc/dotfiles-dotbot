9b5ced361087caf41e217ab855e580a098428a80
Morgan Courbet <ping@morgan.ploki.fr>
2018-03-06 14:45:28 +0100

    Add Firefox addons installation

---
+++ firefox/install-addons.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+addonsId=""
+
+# DuckDuckGo Privacy Essentials - https://addons.mozilla.org/firefox/addon/duckduckgo-for-firefox/
+addonsId="$addonsId 880103"
+# Firefox Multi-Account Containers - https://addons.mozilla.org/firefox/addon/multi-account-containers/
+addonsId="$addonsId 872069"
+# First Party Isolation - https://addons.mozilla.org/firefox/addon/first-party-isolation/
+addonsId="$addonsId 792833"
+# KeePassXC-Browser - https://addons.mozilla.org/firefox/addon/keepassxc-browser/
+addonsId="$addonsId 881663"
+# HTTPS Everywhere - https://addons.mozilla.org/firefox/addon/https-everywhere/
+addonsId="$addonsId 875902"
+# Privacy Badger - https://addons.mozilla.org/firefox/addon/privacy-badger17/
+addonsId="$addonsId 855336"
+# Proxy SwitchyOmega - https://addons.mozilla.org/firefox/addon/switchyomega/
+addonsId="$addonsId 848109"
+
+
+# by default, we assume firefox is in PATH
+firefoxPath="firefox"
+
+if [[ "$#" -gt 1 ]]; then
+    echo "Usage: $0 [firefoxpath]"
+    echo "    firefoxpath: The path to the Firefox binary. Default : $firefoxPath"
+    exit
+fi
+
+if [[ -n "$1" ]]; then
+    if [[ ! -f "$1" && ! -x "$1" ]]; then
+        echo "firefoxpath needs to be an executable file."
+        exit
+    fi
+    firefoxPath="$1"
+fi
+
+
+addonsBaseUrl="https://addons.mozilla.org/firefox/downloads/file/"
+addonsUrlList=""
+for id in $addonsId; do
+    addonsUrlList="$addonsUrlList $addonsBaseUrl$id"
+done
+
+"$firefoxPath" $addonsUrlList
