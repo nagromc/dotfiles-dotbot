e059da74f501e142ac13ccef917ec332f6185966
Morgan Courbet <ping@morgan.ploki.fr>
2018-10-09 16:02:31 +0200

    Add `nsl` alias

--- msdos/aliases.cmd
+++ msdos/aliases.cmd
@@ -4,6 +4,7 @@ doskey cd=cd /d $*
 doskey g=git $*
 doskey gpgcs=gpg --card-status
 doskey ls=dir /b $*
+doskey nsl=nslookup $*
 doskey showaliases=doskey /m
 doskey showpath=@echo %%PATH:;= ^& echo.%%
 doskey x=exit
