f62203039682751966494dff270f498f7da6a4cc
Morgan Courbet <ping@morgan.ploki.fr>
2019-01-25 19:16:42 +0100

    feat(vscode): add Git config language

--- code/install-extentions.sh
+++ code/install-extentions.sh
@@ -3,6 +3,7 @@
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.editorconfig
 code --install-extension HookyQR.beautify
+code --install-extension sidneys1.gitconfig
 code --install-extension timonwong.shellcheck
 code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-maven
