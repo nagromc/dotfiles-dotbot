8874a5a022a5f0a015cd5a0f701652af55b466ae
Morgan Courbet <ping@morgan.ploki.fr>
2019-02-25 09:38:18 +0100

    feat(vscode): add `ryu1kn.partial-diff` extension

--- code/install-extentions.sh
+++ code/install-extentions.sh
@@ -3,6 +3,7 @@
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.editorconfig
 code --install-extension HookyQR.beautify
+code --install-extension ryu1kn.partial-diff
 code --install-extension sidneys1.gitconfig
 code --install-extension timonwong.shellcheck
 code --install-extension vscjava.vscode-java-pack
